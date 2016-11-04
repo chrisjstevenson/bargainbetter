dcc00488b13fdb0b3283fd6197c5978e11661a3c
chrisjstevenson <chris.j.stevenson@gmail.com>
2016-11-04 10:50:34 -0500

    fixing that stupid highlighted menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,48 @@
-/* needs to be run onclick */
-function highlight(route) {
-    if(/howitworks/.test(route)) {
-        $("li").removeClass('active');
-        $("li:nth-child(2)").addClass('active');
-    }
-    if(/about/.test(route)) {
-        $("li").removeClass('active');
-        $("li:nth-child(3)").addClass('active');
-    }
-    if(/blog/.test(route)) {
-        $("li").removeClass('active');
-        $("li:nth-child(4)").addClass('active');
-    }
-    if(/newsletter/.test(route)) {
-        $("li").removeClass('active');
-        $("li:nth-child(5)").addClass('active');
-    }
-    if(/contact/.test(route)) {
-        $("li").removeClass('active');
-        $("li:nth-child(6)").addClass('active');
-    }
-};
+///* needs to be run onclick */
+//function highlight(route) {
+//    var route = window.location.pathname;
+//
+//    if(/#howitworks/.test(route)) {
+//        $("li").removeClass('active');
+//        $("li:nth-child(2)").addClass('active');
+//    }
+//    if(/about/.test(route)) {
+//        $("li").removeClass('active');
+//        $("li:nth-child(3)").addClass('active');
+//    }
+//    if(/blog/.test(route)) {
+//        $("li").removeClass('active');
+//        $("li:nth-child(4)").addClass('active');
+//    }
+//    if(/info/.test(route)) {
+//        $("li").removeClass('active');
+//        $("li:nth-child(5)").addClass('active');
+//    }
+//    if(/contact/.test(route)) {
+//        $("li").removeClass('active');
+//        $("li:nth-child(6)").addClass('active');
+//    }
+//};
 
-//function scrollToAnchor(aid){
-//    var aTag = $("a[name='"+ aid +"']");
-//    $('html,body').animate({ scrollTop: aTag.offset().top },'fast');
-//}
-//
-//$("li:nth-child(2)").click(function() {
-//    scrollToAnchor('howitworks');
-//});
-//
-//$("li:nth-child(3)").click(function() {
-//    scrollToAnchor('about');
-//});
+
+
+
+$(function() {
+    var url = window.location.href;
+
+    // passes on every "a" tag
+    $(".nav a").each(function() {
+
+        // checks if its the same on the address bar
+        if (url == (this.href)) {
+            $("li").removeClass('active');
+            $(this).closest("li").addClass("active");
+        }
+    });
+
+    $(".nav a").click(function() {
+       //console.log($(this));
+       $("li").removeClass('active');
+       $(this).closest("li").addClass("active");
+   });
+});
